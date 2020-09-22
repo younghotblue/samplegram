a552a197ff6efe3a6074022d7041b667caa5bb76
younghotblue <sho.truecolors@gmail.com>
2020-09-22 07:09:05 +0000

    Add password reset

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rails',                   '6.0.3'
+gem 'devise'
 gem 'ransack'
 gem 'enum_help'
 gem 'bcrypt',                  '3.1.13'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "アカウント有効化"
   end
 
-  def password_reset
-    @greeting = "こんにちは"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "パスワード再設定"
   end
 end
