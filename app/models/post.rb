fa433da31dde242c82d6e93286ed996463d34db0
younghotblue <sho.truecolors@gmail.com>
2020-10-09 15:03:44 +0000

    fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -101,22 +101,26 @@ class Post < ApplicationRecord
   after_create do
     post = Post.find_by(id: id)
     # hashbodyに打ち込まれたハッシュタグを検出
-    hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    hashtags.uniq.map do |hashtag|
-      # ハッシュタグは先頭の#を外した上で保存
-      tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
-      post.hashtags << tag
+    if post.hashbody
+      hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+      hashtags.uniq.map do |hashtag|
+        # ハッシュタグは先頭の#を外した上で保存
+        tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
+        post.hashtags << tag
+      end
     end
   end
   
   #更新アクション
   before_update do
     post = Post.find_by(id: id)
-    post.hashtags.clear
-    hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    hashtags.uniq.map do |hashtag|
-      tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
-      post.hashtags << tag
+    if post.hashbody
+      post.hashtags.clear
+      hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
+      hashtags.uniq.map do |hashtag|
+        tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
+        post.hashtags << tag
+      end
     end
   end
   
