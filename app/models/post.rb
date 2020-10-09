7363ada573280e403ab54d5ec8d0ef8e6bac72b0
younghotblue <sho.truecolors@gmail.com>
2020-10-09 16:17:53 +0000

    fix

--- app/models/post.rb
+++ app/models/post.rb
@@ -114,30 +114,6 @@ class Post < ApplicationRecord
   #更新アクション
   before_update do
     post = Post.find_by(id: id)
-    if post.hashbody?
-      post.hashtags.clear
-      hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-      hashtags.uniq.map do |hashtag|
-        tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
-        post.hashtags << tag
-      end
-    end
-  end
-  
-  after_create do
-    post = Post.find_by(id: id)
-    # hashbodyに打ち込まれたハッシュタグを検出
-    hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
-    hashtags.uniq.map do |hashtag|
-      # ハッシュタグは先頭の#を外した上で保存
-      tag = Hashtag.find_or_create_by(hashname: hashtag.downcase.delete('#'))
-      post.hashtags << tag
-    end
-  end
-  
-  #更新アクション
-  before_update do
-    post = Post.find_by(id: id)
     post.hashtags.clear
     hashtags = hashbody.scan(/[#＃][\w\p{Han}ぁ-ヶｦ-ﾟー]+/)
     hashtags.uniq.map do |hashtag|
