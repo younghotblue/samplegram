ffd7d608a6a2b81f61a427d55d0aeecdc6e91898
younghotblue <sho.truecolors@gmail.com>
2020-10-19 16:02:03 +0000

    Improved notification function

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -12,12 +12,12 @@ module NotificationsHelper
       #aタグで通知を作成したユーザーshowのリンクを作成
       tag.a(notification.visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + 'があなたをフォローしました。'
     when 'like'
-      tag.a(notification.visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + 'が' + tag.a('あなたの投稿', href: "#", style: "font-weight: bold;") + 'に「いいね！」しました。'
+      tag.a(notification.visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + 'が' + tag.a('あなたの投稿', href: post_path(notification.post_id), style: "font-weight: bold;") + 'に「いいね！」しました。'
     when 'comment' then
       #コメントの内容と投稿のタイトルを取得　      
       @comment = Comment.find_by(id: notification.comment_id)&.comment
       @comment_content = @comment
-      tag.a(@visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + 'が' + tag.a('あなたの投稿', href: "#", style: "font-weight: bold;") + 'にコメントしました。'
+      tag.a(@visitor.name, href: user_path(@visitor), style: "font-weight: bold;") + 'が' + tag.a('あなたの投稿', href: post_path(notification.post_id), style: "font-weight: bold;") + 'にコメントしました。'
     end
   end
 end
